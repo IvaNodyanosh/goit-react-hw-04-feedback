88a95d64fbe2b848219b7e597cca0e0b9c8c4a7a
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2024-01-18 17:31:37 +0200

    ending task

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,44 +1,49 @@
-import { Component } from "react";
-import { countTotalFeedback } from "./function/countTotalFeedback";
-import { countPositiveFeedbackPercentage } from "./function/countPositiveFeedbackPercentage";
-import { Statistics } from "./statistics/statistics";
-import { FeedbackOptions } from "./feedbackOptions/feedbackOptions";
-import { Section } from "./sections/sections";
-import { Notification } from "./notification/notification";
-import css from "./feedback.module.css"
+import { useReducer } from 'react';
+import { countTotalFeedback } from './function/countTotalFeedback';
+import { countPositiveFeedbackPercentage } from './function/countPositiveFeedbackPercentage';
+import { Statistics } from './statistics/statistics';
+import { FeedbackOptions } from './feedbackOptions/feedbackOptions';
+import { Section } from './sections/sections';
+import { Notification } from './notification/notification';
+import css from './feedback.module.css';
 
-export class Feedback extends Component {
-    
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
-
-    addFeedback = (e) => {
-        const {name} = e.currentTarget
-    
-        this.setState(prevState => ({ [name]: prevState[name] + 1 }));
-    
-    }
-
-
-    render() { 
-        const total = countTotalFeedback(this.state);
-        const positivePercentage = countPositiveFeedbackPercentage(total, this.state);
-        return(
-            <div className={css.container}>
-                <Section title={"Please leave feedback"}>
-                    <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.addFeedback} />
-                </Section>
-                
-                <Section title={"Statistics"}>
-                    {total>0 ? <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={total} positivePercentage={positivePercentage} /> : <Notification message={"There is no feedback"}/>}
-                   
-                </Section>
-
-            </div>
-        )
-    }
-
+function reducer(prevFeedback, { name }) {
+  return { ...prevFeedback, [name]: prevFeedback[name] + 1 };
 }
+
+export const Feedback = () => {
+ const [feedback, changeFeedback] = useReducer(reducer, { good: 0, neutral: 0, bad: 0 });
+
+  const addFeedback = e => {
+    const { name } = e.currentTarget;
+
+    changeFeedback({ name: name });
+  };
+
+  const total = countTotalFeedback(feedback);
+  const positivePercentage = countPositiveFeedbackPercentage(total, feedback);
+  return (
+    <div className={css.container}>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={addFeedback}
+        />
+      </Section>
+
+      <Section title={'Statistics'}>
+        {total > 0 ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message={'There is no feedback'} />
+        )}
+      </Section>
+    </div>
+  );
+};
