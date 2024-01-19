2cc4ff9dbf4bf2cd1bf42f705b6d0647e99eb672
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2024-01-19 11:34:04 +0200

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,37 @@
-import { useReducer } from 'react';
-import { countTotalFeedback } from './function/countTotalFeedback';
-import { countPositiveFeedbackPercentage } from './function/countPositiveFeedbackPercentage';
+import { useState } from 'react';
 import { Statistics } from './statistics/statistics';
 import { FeedbackOptions } from './feedbackOptions/feedbackOptions';
 import { Section } from './sections/sections';
 import { Notification } from './notification/notification';
-import css from './feedback.module.css';
-
-function reducer(prevFeedback, { name }) {
-  return { ...prevFeedback, [name]: prevFeedback[name] + 1 };
-}
+import css from './App.module.css';
 
 export const Feedback = () => {
- const [feedback, changeFeedback] = useReducer(reducer, { good: 0, neutral: 0, bad: 0 });
+  const [feedback, changeFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const addFeedback = e => {
     const { name } = e.currentTarget;
 
-    changeFeedback({ name: name });
+    changeFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [name]: prevFeedback[name] + 1,
+    }));
+  };
+
+  const countPositiveFeedbackPercentage = (total, { good }) => {
+    if (total === 0) {
+      return 0;
+    }
+    return ((good / total) * 100).toFixed(0);
+  };
+
+  const countTotalFeedback = totalObject => {
+    let total = 0;
+
+    for (const key in totalObject) {
+      total += totalObject[key];
+    }
+
+    return total;
   };
 
   const total = countTotalFeedback(feedback);
--- src/components/feedbackOptions/feedbackOptions.jsx
+++ src/components/feedbackOptions/feedbackOptions.jsx
@@ -1,17 +1,29 @@
-import css from "./feedbackOptions.module.css"
+import css from './feedbackOptions.module.css';
 
-import { nanoid } from "nanoid";
-
-import { BsFillEmojiSmileFill } from "react-icons/bs";
-import { BsFillEmojiNeutralFill } from "react-icons/bs";
-import { BsFillEmojiFrownFill } from "react-icons/bs";
+import { BsFillEmojiSmileFill } from 'react-icons/bs';
+import { BsFillEmojiNeutralFill } from 'react-icons/bs';
+import { BsFillEmojiFrownFill } from 'react-icons/bs';
 
 const emoji = {
-    good: <BsFillEmojiSmileFill className={css.feedbackIcon__good} />,
-    neutral: <BsFillEmojiNeutralFill className={css.feedbackIcon__neutral} />,
-    bad: <BsFillEmojiFrownFill className={css.feedbackIcon__bad} />,
-}
+  good: <BsFillEmojiSmileFill className={css.feedbackIcon__good} />,
+  neutral: <BsFillEmojiNeutralFill className={css.feedbackIcon__neutral} />,
+  bad: <BsFillEmojiFrownFill className={css.feedbackIcon__bad} />,
+};
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return <ul className={css.feedback__list}>{options.map(option =><li key={nanoid()}><button onClick={(e) => onLeaveFeedback(e)} name={option} className={css.feedback__button} >{emoji[option]}</button></li>) }</ul>
-}
+  return (
+    <ul className={css.feedback__list}>
+      {options.map(option => (
+        <li key={option}>
+          <button
+            onClick={e => onLeaveFeedback(e)}
+            name={option}
+            className={css.feedback__button}
+          >
+            {emoji[option]}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
